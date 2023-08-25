8f95d83ab8915206373bc0ca3f5b47307f2ce5cf
NirfikLoopMe <54435539+Nirfikys@users.noreply.github.com>
2023-08-25 13:58:56 +0200

    fix rewarded in IS demo app

--- bridges/ironSorce/ironsorce-mediation-sample/ironsource-73/src/main/java/com/loopme/ironsource_73/RewardedVideoActivity.java
+++ bridges/ironSorce/ironsorce-mediation-sample/ironsource-73/src/main/java/com/loopme/ironsource_73/RewardedVideoActivity.java
@@ -5,19 +5,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.ironsource.adapters.custom.loopme.LoopmeCustomAdapter;
 import com.ironsource.mediationsdk.IronSource;
 import com.ironsource.mediationsdk.adunit.adapter.utility.AdInfo;
 import com.ironsource.mediationsdk.logger.IronSourceError;
 import com.ironsource.mediationsdk.model.Placement;
-import com.ironsource.mediationsdk.sdk.LevelPlayInterstitialListener;
-import com.ironsource.mediationsdk.sdk.LevelPlayRewardedVideoListener;
-import com.ironsource.mediationsdk.sdk.RewardedVideoListener;
+import com.ironsource.mediationsdk.sdk.LevelPlayRewardedVideoManualListener;
 import com.loopme.LoopMeSdk;
 
 public class RewardedVideoActivity extends Activity {
     private static final String appKey = "124e1d38d";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,60 +38,46 @@ public class RewardedVideoActivity extends Activity {
                 Toast.makeText(RewardedVideoActivity.this, "Loopme failed initialization", Toast.LENGTH_LONG).show();
             }
         });
-        IronSource.init(this, appKey, IronSource.AD_UNIT.REWARDED_VIDEO);
-        IronSource.setAdaptersDebug(true);
-
-        Toast.makeText(this, "IronSource has been initialized", Toast.LENGTH_SHORT).show();
-        IronSource.setLevelPlayRewardedVideoListener(new LevelPlayRewardedVideoListener() {
-            // Indicates that there's an available ad.
-            // The adInfo object includes information about the ad that was loaded successfully
-            // Use this callback instead of onRewardedVideoAvailabilityChanged(true)
+        IronSource.setLevelPlayRewardedVideoManualListener(new LevelPlayRewardedVideoManualListener() {
             @Override
-            public void onAdAvailable(AdInfo adInfo) {
-                Toast.makeText(getApplicationContext(), "onRewardedAdAvailable", Toast.LENGTH_SHORT).show();
+            public void onAdReady(AdInfo adInfo) {
+                Toast.makeText(getApplicationContext(), "onRewardedAdReady", Toast.LENGTH_SHORT).show();
             }
 
-            // Indicates that no ads are available to be displayed
-            // Use this callback instead of onRewardedVideoAvailabilityChanged(false)
             @Override
-            public void onAdUnavailable() {
-                Toast.makeText(getApplicationContext(), "onRewardedVideoAdUnavailable", Toast.LENGTH_SHORT).show();
+            public void onAdLoadFailed(IronSourceError ironSourceError) {
+                Toast.makeText(getApplicationContext(), "onRewardedLoadFailed", Toast.LENGTH_SHORT).show();
             }
 
-            // The Rewarded Video ad view has opened. Your activity will loose focus
             @Override
             public void onAdOpened(AdInfo adInfo) {
-                Toast.makeText(getApplicationContext(), "onRewardedVideoAdOpened", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "onRewardedAdOpened", Toast.LENGTH_SHORT).show();
             }
 
-            // The Rewarded Video ad view is about to be closed. Your activity will regain its focus
             @Override
-            public void onAdClosed(AdInfo adInfo) {
-                Toast.makeText(getApplicationContext(), "onRewardedVideoAdClosed", Toast.LENGTH_SHORT).show();
+            public void onAdShowFailed(IronSourceError ironSourceError, AdInfo adInfo) {
+                Toast.makeText(getApplicationContext(), "onRewardedAdShowFailed", Toast.LENGTH_SHORT).show();
             }
 
-            // The user completed to watch the video, and should be rewarded.
-            // The placement parameter will include the reward data.
-            // When using server-to-server callbacks, you may ignore this event and wait for the ironSource server callback
-            @Override
-            public void onAdRewarded(Placement placement, AdInfo adInfo) {
-                Toast.makeText(getApplicationContext(), "onRewardedVideoAdRewarded", Toast.LENGTH_SHORT).show();
-            }
-
-            // The rewarded video ad was failed to show
-            @Override
-            public void onAdShowFailed(IronSourceError error, AdInfo adInfo) {
-                Toast.makeText(getApplicationContext(), "onRewardedVideoShowFailed", Toast.LENGTH_SHORT).show();
-            }
-
-            // Invoked when the video ad was clicked.
-            // This callback is not supported by all networks, and we recommend using it
-            // only if it's supported by all networks you included in your build
             @Override
             public void onAdClicked(Placement placement, AdInfo adInfo) {
-                Toast.makeText(getApplicationContext(), "onRewardedVideoAdClicked", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "onRewardedAdClicked", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdRewarded(Placement placement, AdInfo adInfo) {
+                Toast.makeText(getApplicationContext(), "onRewardedAdRewarded", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onAdClosed(AdInfo adInfo) {
+                Toast.makeText(getApplicationContext(), "onRewardedAdClosed", Toast.LENGTH_SHORT).show();
             }
         });
+
+        IronSource.init(this, appKey, IronSource.AD_UNIT.REWARDED_VIDEO);
+        IronSource.setAdaptersDebug(true);
+        Toast.makeText(this, "IronSource has been initialized", Toast.LENGTH_SHORT).show();
     }
 
     public void onShowClicked(View view) {
@@ -102,7 +86,7 @@ public class RewardedVideoActivity extends Activity {
     }
 
     public void onLoadClicked(View view) {
-        String loopmeAppkey = ((EditText)findViewById(R.id.appkey_et)).getText().toString();
+        String loopmeAppkey = ((EditText) findViewById(R.id.appkey_et)).getText().toString();
         LoopmeCustomAdapter.setWeakActivity(this);
         LoopmeCustomAdapter.setLoopmeAppkey(loopmeAppkey);
         IronSource.loadRewardedVideo();
