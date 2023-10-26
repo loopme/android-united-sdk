547057107ed2600ee91d2e0051a80c4b8ee8a926
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2023-10-26 10:52:00 +0300

    Add ironsource-75 subproject

---
+++ bridges/ironSorce/ironsorce-mediation-sample/ironsource-75/src/main/java/com/ironsource/adapters/custom/loopme/LoopmeCustomRewardedVideo.java
@@ -0,0 +1,93 @@
+package com.ironsource.adapters.custom.loopme;
+
+import android.app.Activity;
+
+import androidx.annotation.Keep;
+
+import com.ironsource.mediationsdk.adunit.adapter.BaseRewardedVideo;
+import com.ironsource.mediationsdk.adunit.adapter.listener.RewardedVideoAdListener;
+import com.ironsource.mediationsdk.adunit.adapter.utility.AdData;
+import com.ironsource.mediationsdk.adunit.adapter.utility.AdapterErrorType;
+import com.ironsource.mediationsdk.model.NetworkSettings;
+import com.loopme.IntegrationType;
+import com.loopme.LoopMeInterstitial;
+import com.loopme.common.LoopMeError;
+
+import org.jetbrains.annotations.NotNull;
+
+@Keep
+public class LoopmeCustomRewardedVideo extends BaseRewardedVideo<LoopmeCustomAdapter> {
+
+    private static final String LOG_TAG = LoopmeCustomRewardedVideo.class.getSimpleName();
+
+    private LoopMeInterstitial mInterstitial;
+
+    private RewardedVideoAdListener rewardedVideoAdListener;
+
+    public LoopmeCustomRewardedVideo(NetworkSettings networkSettings) {
+        super(networkSettings);
+    }
+
+    @Override
+    public void loadAd(AdData adData, Activity activity, @NotNull RewardedVideoAdListener rewardedVideoAdListener) {
+        try {
+            this.rewardedVideoAdListener = rewardedVideoAdListener;
+            String appkey = ((LoopmeCustomAdapter) getNetworkAdapter()).getLoopmeAppkey();
+            mInterstitial = LoopMeInterstitial.getInstance(appkey, activity);
+            mInterstitial.setAutoLoading(false);
+            mInterstitial.setListener(new LoopMeInterstitial.Listener() {
+                @Override
+                public void onLoopMeInterstitialClicked(LoopMeInterstitial arg0) {
+                    rewardedVideoAdListener.onAdClicked();
+                }
+
+                @Override
+                public void onLoopMeInterstitialExpired(LoopMeInterstitial arg0) {
+                }
+
+                @Override
+                public void onLoopMeInterstitialHide(LoopMeInterstitial arg0) {
+                }
+
+                @Override
+                public void onLoopMeInterstitialLeaveApp(LoopMeInterstitial arg0) {
+                }
+
+                @Override
+                public void onLoopMeInterstitialLoadSuccess(LoopMeInterstitial arg0) {
+                    rewardedVideoAdListener.onAdLoadSuccess();
+                }
+
+                @Override
+                public void onLoopMeInterstitialLoadFail(LoopMeInterstitial loopMeInterstitial, LoopMeError i) {
+                    rewardedVideoAdListener.onAdLoadFailed(AdapterErrorType.ADAPTER_ERROR_TYPE_INTERNAL, i.getErrorCode(), i.getMessage());
+                }
+
+                @Override
+                public void onLoopMeInterstitialShow(LoopMeInterstitial arg0) {
+                    rewardedVideoAdListener.onAdShowSuccess();
+                }
+
+                @Override
+                public void onLoopMeInterstitialVideoDidReachEnd(LoopMeInterstitial interstitial) {
+                    rewardedVideoAdListener.onAdEnded();
+                    rewardedVideoAdListener.onAdRewarded();
+                }
+            });
+            mInterstitial.load(IntegrationType.NORMAL);
+        } catch (Exception e) {
+            rewardedVideoAdListener.onAdLoadFailed(AdapterErrorType.ADAPTER_ERROR_TYPE_INTERNAL, -1, e.getMessage());
+        }
+    }
+
+    @Override
+    public void showAd(AdData adData, RewardedVideoAdListener rewardedVideoAdListener) {
+        mInterstitial.show();
+    }
+
+    @Override
+    public boolean isAdAvailable(AdData adData) {
+        return mInterstitial.isReady();
+    }
+
+}
---
+++ bridges/ironSorce/ironsorce-mediation-sample/ironsource-75/src/main/java/com/loopme/ironsource_75/InterstitialActivity.java
@@ -0,0 +1,130 @@
+package com.loopme.ironsource_75;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.ironsource.adapters.custom.loopme.LoopmeCustomAdapter;
+import com.ironsource.mediationsdk.IronSource;
+import com.ironsource.mediationsdk.adunit.adapter.utility.AdInfo;
+import com.ironsource.mediationsdk.logger.IronSourceError;
+import com.ironsource.mediationsdk.sdk.LevelPlayInterstitialListener;
+import com.loopme.LoopMeSdk;
+
+public class InterstitialActivity extends Activity {
+    private static final String appKey = "124e1d38d";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interstitial);
+        View loadButton = findViewById(R.id.load_button);
+        loadButton.setEnabled(false);
+        // Use this method in case if you Publisher is willing to ask GDPR consent with your own or
+        // MoPub dialog AND don't want LoopMe consent dialog to be shown - pass GDPR consent to this method.
+        //LoopMeAdapterConfiguration.setPublisherConsent(loopMeConf, true);
+
+        LoopMeSdk.initialize(this, new LoopMeSdk.Configuration(), new LoopMeSdk.LoopMeSdkListener() {
+            @Override
+            public void onSdkInitializationSuccess() {
+                Toast.makeText(InterstitialActivity.this, "Loopme has been initialized", Toast.LENGTH_LONG).show();
+                loadButton.setEnabled(true);
+            }
+
+            @Override
+            public void onSdkInitializationFail(int error, String message) {
+                Toast.makeText(InterstitialActivity.this, "Loopme failed initialization", Toast.LENGTH_LONG).show();
+            }
+        });
+        IronSource.init(this, appKey, IronSource.AD_UNIT.INTERSTITIAL);
+        IronSource.setAdaptersDebug(true);
+
+        Toast.makeText(this, "IronSource has been initialized", Toast.LENGTH_SHORT).show();
+
+        IronSource.setLevelPlayInterstitialListener(new LevelPlayInterstitialListener() {
+            /**
+             * Invoked when Interstitial Ad is ready to be shown after load function was called.
+             */
+            @Override
+            public void onAdReady(AdInfo adInfo) {
+                Toast.makeText(getApplicationContext(), "onInterstitialAdReady", Toast.LENGTH_SHORT).show();
+            }
+
+            /**
+             * invoked when there is no Interstitial Ad available after calling load function.
+             */
+            @Override
+            public void onAdLoadFailed(IronSourceError ironSourceError) {
+                Toast.makeText(getApplicationContext(), "onInterstitialAdLoadFailed", Toast.LENGTH_SHORT).show();
+            }
+
+            /**
+             * Invoked when the Interstitial Ad Unit is opened
+             */
+            @Override
+            public void onAdOpened(AdInfo adInfo) {
+                Toast.makeText(getApplicationContext(), "onInterstitialAdOpened", Toast.LENGTH_SHORT).show();
+            }
+
+            /** Invoked right before the Interstitial screen is about to open.
+             *  NOTE - This event is available only for some of the networks.
+             *  You should NOT treat this event as an interstitial impression, but rather use InterstitialAdOpenedEvent
+             */
+            @Override
+            public void onAdShowSucceeded(AdInfo adInfo) {
+                Toast.makeText(getApplicationContext(), "onInterstitialAdShowSucceeded", Toast.LENGTH_SHORT).show();
+            }
+
+            /**
+             * Invoked when Interstitial ad failed to show.
+             * @param ironSourceError - An object which represents the reason of showInterstitial failure.
+             */
+            @Override
+            public void onAdShowFailed(IronSourceError ironSourceError, AdInfo adInfo) {
+                Toast.makeText(getApplicationContext(), "onInterstitialAdShowFailed", Toast.LENGTH_SHORT).show();
+            }
+
+            /**
+             * Invoked when the end user clicked on the interstitial ad, for supported networks only.
+             */
+            @Override
+            public void onAdClicked(AdInfo adInfo) {
+                Toast.makeText(getApplicationContext(), "onInterstitialAdClicked", Toast.LENGTH_SHORT).show();
+            }
+
+            /**
+             * Invoked when the ad is closed and the user is about to return to the application.
+             */
+            @Override
+            public void onAdClosed(AdInfo adInfo) {
+                Toast.makeText(getApplicationContext(), "onInterstitialAdClosed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void onShowClicked(View view) {
+        if (IronSource.isInterstitialReady())
+            IronSource.showInterstitial();
+    }
+
+    public void onLoadClicked(View view) {
+        String loopmeAppkey = ((EditText)findViewById(R.id.appkey_et)).getText().toString();
+        LoopmeCustomAdapter.setWeakActivity(this);
+        LoopmeCustomAdapter.setLoopmeAppkey(loopmeAppkey);
+        IronSource.loadInterstitial();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IronSource.onResume(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        IronSource.onPause(this);
+    }
+}
