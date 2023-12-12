74a69cb9630dc2b4b69a3a7472a9b38b05258ebe
Mateusz Polański <mateusz.polanski@loopme.com>
2023-12-12 11:20:03 +0100

    Add API check annotations and modify sample-banner app manifest

--- loopme-sdk/src/main/java/com/loopme/utils/ApiLevel.java
+++ loopme-sdk/src/main/java/com/loopme/utils/ApiLevel.java
@@ -2,19 +2,24 @@ package com.loopme.utils;
 
 import android.os.Build;
 
+import androidx.annotation.ChecksSdkIntAtLeast;
+
 public class ApiLevel {
 
     private ApiLevel() {
     }
 
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.LOLLIPOP)
     public static boolean isApi21AndHigher() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
     }
 
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.N)
     public static boolean isApi24AndHigher() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.N;
     }
 
+    @ChecksSdkIntAtLeast(api = Build.VERSION_CODES.O)
     public static boolean isApi26AndHigher() {
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.O;
     }
