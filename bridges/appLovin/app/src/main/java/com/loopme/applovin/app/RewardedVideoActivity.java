f0a989d3f30b2b8d73471f3f88c6096ef680d050
Brandon Zarzoza <brandon.zarzoza@loopme.com>
2024-04-30 14:54:20 -0700

    Add rewarded ad to rewarded activity for max test app

--- bridges/appLovin/app/src/main/java/com/loopme/applovin/app/RewardedVideoActivity.java
+++ bridges/appLovin/app/src/main/java/com/loopme/applovin/app/RewardedVideoActivity.java
@@ -5,27 +5,83 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.applovin.mediation.MaxAd;
+import com.applovin.mediation.MaxError;
+import com.applovin.mediation.MaxReward;
+import com.applovin.mediation.MaxRewardedAdListener;
+import com.applovin.mediation.ads.MaxRewardedAd;
 import com.loopme.LoopMeSdk;
 
-public class RewardedVideoActivity extends Activity {
-
+public class RewardedVideoActivity extends Activity implements MaxRewardedAdListener {
+    private MaxRewardedAd rewardedAd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_interstitial);
+        setContentView(R.layout.activity_rewarded);
         View loadButton = findViewById(R.id.load_button);
         loadButton.setEnabled(false);
-        LoopMeSdk.initialize(this, new LoopMeSdk.Configuration(), new LoopMeSdk.LoopMeSdkListener() {
-            @Override
-            public void onSdkInitializationSuccess() {
-                Toast.makeText(RewardedVideoActivity.this, "Loopme has been initialized", Toast.LENGTH_LONG).show();
-                loadButton.setEnabled(true);
-            }
+        rewardedAd = MaxRewardedAd.getInstance("53df96fcbd095f70", this);
+        rewardedAd.setListener(this);
+        rewardedAd.loadAd();
+    }
 
-            @Override
-            public void onSdkInitializationFail(int error, String message) {
-                Toast.makeText(RewardedVideoActivity.this, "Loopme failed initialization", Toast.LENGTH_LONG).show();
-            }
-        });
+    public void onLoadClicked(View view) {
+        Toast.makeText(RewardedVideoActivity.this, "POP", Toast.LENGTH_LONG).show();
+
+    }
+    public void onShowClicked(View view) {
+        if ( rewardedAd.isReady() )
+        {
+            Toast.makeText(RewardedVideoActivity.this, "SHOWING", Toast.LENGTH_LONG).show();
+
+            rewardedAd.showAd();
+        } else {
+            Toast.makeText(RewardedVideoActivity.this, "NOT SHOWING", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    @Override
+    public void onUserRewarded(@NonNull MaxAd maxAd, @NonNull MaxReward maxReward) {
+        Toast.makeText(RewardedVideoActivity.this, "onUserRewarded", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onAdLoaded(@NonNull MaxAd maxAd) {
+        Toast.makeText(RewardedVideoActivity.this, "onAdLoaded", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onAdDisplayed(@NonNull MaxAd maxAd) {
+        Toast.makeText(RewardedVideoActivity.this, "onAdDisplayed", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onAdHidden(@NonNull MaxAd maxAd) {
+        Toast.makeText(RewardedVideoActivity.this, "onAdHidden", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onAdClicked(@NonNull MaxAd maxAd) {
+        Toast.makeText(RewardedVideoActivity.this, "onAdClicked", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onAdLoadFailed(@NonNull String s, @NonNull MaxError maxError) {
+        Toast.makeText(RewardedVideoActivity.this, "onAdLoadFailed", Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    public void onAdDisplayFailed(@NonNull MaxAd maxAd, @NonNull MaxError maxError) {
+        Toast.makeText(RewardedVideoActivity.this, "onAdDisplayFailed", Toast.LENGTH_LONG).show();
+
     }
 }
