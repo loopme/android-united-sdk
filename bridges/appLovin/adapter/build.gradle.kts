270d14f52187637ec344d7112a4b7d6f307dd124
Evgen A. Epanchin <EpanchinE@gmail.com>
2024-05-20 11:48:57 +0300

    Use local LoopMe SDK version

--- bridges/appLovin/adapter/build.gradle.kts
+++ bridges/appLovin/adapter/build.gradle.kts
@@ -11,8 +11,8 @@ android {
 }
 
 dependencies {
-//    api(project(":loopme-sdk"))
-    api("com.github.loopme.android-united-sdk:loopme-sdk:9.0.0")
+    api(project(":loopme-sdk"))
+//    api("com.github.loopme.android-united-sdk:loopme-sdk:9.0.0")
     api("com.applovin:applovin-sdk:12.4.3")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
