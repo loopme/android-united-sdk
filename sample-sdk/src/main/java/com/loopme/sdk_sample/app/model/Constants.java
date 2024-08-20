59a6d31bcb755f3b704e1ba5a4326b617ce6df01
nowakw <wojciech.nowak@loopme.com>
2024-08-20 10:27:09 +0200

    [SDK-378] Remove deprecated files and unused resources

--- sample-sdk/src/main/java/com/loopme/sdk_sample/app/model/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.loopme.sdk_sample.app.model;
-
-public class Constants {
-    public static final String SIMPLE = "Simple";
-    public static final String RECYCLERVIEW = "RecyclerView";
-}
